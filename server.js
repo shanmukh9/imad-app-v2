5cb208be9bf85a472f4448e99f6f05d385fddeb2
YSS <yalavarthisainadh@gmail.com>
2017-05-16 13:47:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/first-page',function(req,res){
-    res.send('page is served ');
+    res.sendFile(path.join(__dirname, 'ui', 'first-page.html'));
 });
 
 
