6f312593e37294dd0e695ebbd8f8decf175bfa36
YSS <yalavarthisainadh@gmail.com>
2017-05-18 14:28:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
 app.get('/counter',function (req,res) {
     counter=counter+1;
    res.sendFile(counter.toString());
